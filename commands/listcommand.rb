ae6ca4c48a1b85575d49f4f7c7dbc305d6f1971f
Peter Wilhelmsson <2hdddg@gmail.com>
2012-10-10 20:56:47 +0200

    Prettier output from list command

--- commands/listcommand.rb
+++ commands/listcommand.rb
@@ -6,7 +6,8 @@ class ListCommand
 
 	def run(parameters, input_callback, output_callback)
 		@repository.all do |f, m| 
-			output_callback.call "#{m.title} [#{f}]" 
+			formatted = sprintf('%-30s %-30s', m.title[0, 30], File.basename(f)[0, 30])
+			output_callback.call formatted 
 		end
 	end
 end
