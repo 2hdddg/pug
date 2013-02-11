b55859a454608f73b2367c982f567d8dc0623662
2hdddg <2hdddg@gmail.com>
2013-02-11 20:38:23 +0100

    Fixed failing test after making template path configurable

--- test/tc_listcommand.rb
+++ test/tc_listcommand.rb
@@ -2,7 +2,9 @@ require 'test/unit'
 
 $:.unshift(File.expand_path('../../', __FILE__))
 require "models/bug"
-require 'commands/listcommand'
+require "commands/listcommand"
+require "models/userconfiguration"
+require "models/globalconfiguration"
 
 class TestListCommand < Test::Unit::TestCase
 	
@@ -20,7 +22,10 @@ class TestListCommand < Test::Unit::TestCase
 	def test_run_should_output_title_of_all_bugs
 		bugs = []
 		fake = FakeRepository.new
-		listcommand = Commands::ListCommand.new(fake, nil, nil)
+		userconfiguration = Models::UserConfiguration.new
+		globalconfiguration = Models::GlobalConfiguration.new
+		globalconfiguration.template_dir = './templates'
+		listcommand = Commands::ListCommand.new(fake, userconfiguration, globalconfiguration)
 		commandcontext = Commands::CommandContext.new(['add'], lambda {|o| bugs.push(o)}, lambda {|name, desc, default| default })
 
 		listcommand.run commandcontext
