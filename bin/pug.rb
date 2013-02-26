42ad8c5bf04497372a28dc6c589fa69eb2d64027
2hdddg <2hdddg@gmail.com>
2013-02-26 20:38:50 +0100

    Removed configuration and initcommand, not needed anymore

--- bin/pug.rb
+++ bin/pug.rb
@@ -1,9 +1,7 @@
 #!/usr/bin/env ruby
 require "pug"
 require "tracker"
-require "configuration"
 require "commands/commandcontext"
-require "commands/initcommand"
 require "commands/addcommand"
 require "commands/helpcommand"
 require "commands/diffcommand"
@@ -26,23 +24,11 @@ end
 commandcontext = Commands::CommandContext.new(ARGV, onerror, onoutput, onprompt, onexit)
 commandname = commandcontext.pop_argument!("Missing command, try help ;-)")
 
-configuration = Configuration.new('.')
-
-# make sure that we are configured
-if commandname == 'init' || !configuration.has_globalconfiguration?
-	puts "There is no configuration available, please provide me with some info..." if commandname != "init"
-	Commands::InitCommand.new(configuration).run commandcontext
-	exit 0 if commandname == 'init'
-end
-
 if commandname == nil
 	commandname = 'help'
 end
 
-globalconfiguration = configuration.get_globalconfiguration()
-
-# path to directory should be read from .pug_global
-pugspath = globalconfiguration.pugspath
+pugspath = File.join('.', 'pugs')
 tracker = Tracker.new(pugspath)
 
 command = Meta::command_from_name(commandname, tracker)
